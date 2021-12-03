95b5844b73e58c3000e7d05cecb1f8b74c92524c
Dallas Strube <strubedallas@gmail.com>
2021-12-03 14:05:25 -0500

    BaseSetUp

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = 8000;
+
+express()
+  .use(express.json())
+
+  .listen(PORT, function () {
+    console.info("🌎 Listening on port " + PORT);
+  });
