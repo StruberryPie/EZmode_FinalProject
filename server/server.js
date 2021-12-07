871b41a56f813edbc5e5fd8acb8cfe40259e73ad
Dallas Strube <strubedallas@gmail.com>
2021-12-06 19:03:57 -0500

    BackEnd

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const PORT = 8000;
 express()
   .use(express.json())
 
+  // api functions
+  /////////////////////////////////////////
+
+  /////////////////////////////////////////
+
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
   .listen(PORT, function () {
     console.info("🌎 Listening on port " + PORT);
   });
