a968b698c101d0b089eb184f480db77b9f85d2d8
Dallas Strube <strubedallas@gmail.com>
2021-12-15 16:10:11 -0500

    FrontEnd

---
+++ client/src/CharacterSheetRW/Personality.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Personality = () => {
+  return (
+    <>
+      <p>
+        Personality Traits
+        <textarea />
+      </p>
+      <p>
+        Ideals
+        <textarea />
+      </p>
+      <p>
+        Bonds
+        <textarea />
+      </p>
+      <p>
+        Flaws
+        <textarea />
+      </p>
+    </>
+  );
+};
+
+export default Personality;
--- client/src/CharacterSheetRW/Traits.js
+++ client/src/CharacterSheetRW/Traits.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
 
 const Traits = () => {
+  const { character } = useContext(UserContext);
+
   return (
     <>
       <p>Traits</p>
+      {character.race_info ? (
+        character.race_info.traits.map((trait) => {
+          return <p>- {trait.name}</p>;
+        })
+      ) : (
+        <p>none</p>
+      )}
     </>
   );
 };
